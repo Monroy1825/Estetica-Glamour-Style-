8a5c4be35135e9152919da4d75bfed31da74a5c3
Monroy1825 <22460314@colima.tecnm.mx>
2026-04-28 14:37:36 -0600

    Limpieza: corrige migraciones duplicadas y elimina archivos basura

--- apps/operaciones/migrations/0006_remove_cita_duracion_minutos_cita_duracion_horas.py
+++ apps/operaciones/migrations/0006_remove_cita_duracion_minutos_cita_duracion_horas.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('operaciones', '0005_cita_duracion_minutos'),
+        ('operaciones', '0008_cita_duracion_minutos'),
     ]
 
     operations = [
